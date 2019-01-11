f3bec66d2e1ecfa13c0802a6bc2a18b6f1514087
amirrezaask <raskarpour@gmail.com>
2019-01-11 22:49:50 +0330

    general functions

--- src/StarWars.php
+++ src/StarWars.php
@@ -1,27 +1,42 @@
 <?php
 
-
-
 namespace FunRest;
 
+class StarWars {
 
-class StarWars
-{
+	private static function get($uri = "") {
+		$baseUrl = "https://swapi.co/api/";
+		$url = $baseUrl . $uri;
+		$resp = \Requests::get($url);
+		return json_encode($resp);
+	}
 
-    function __construct()
-    {
-    }
-    private static function sendGET($uri)
-    {
-        $baseUrl = "https://swapi.co/api/";
-        $url = $baseUrl . $uri;
-        $resp = \Requests::get($url);
-        return $resp;
-    }
+	/**
+	 * gets all available data on this API
+	 *
+	 * @return string encoded json of response 
+	 */
+	static function getAllAvailData() {
+		return self::get();
+	}
+	/**
+	 * gets json schema of given resource name
+	 *
+	 * @param string $resource
+	 * @return void
+	 */
+	static function getSchema(string $resource)
+	{
+		return self::get($resource . "/schema");
+	}
 
+	static function getResourceById(string $resource,int $id)
+	{
+		return self::get($resource . "/" . $id);
+	}
+	static function searchResource(string $resource, string $query)
+	{
+		return self::get($resource . "/?search" . $query);
+	}
 
-    static function getAllAvailData() 
-    {
-        return self::sendGET("");
-    }
 }
